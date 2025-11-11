263348d86f085d5e4fe59b831e0f8d1ad93be6a7
JulianaForbici <julianaforbicii@gmail.com>
2025-11-11 08:29:23 -0300

    Tipos de dados para estudos

---
+++ src/main/java/exemplos/ExemploTiposdeDados.java
@@ -0,0 +1,15 @@
+package exemplos;
+
+public class ExemploTiposdeDados {
+    public static void main(String[] args) {
+        int valor01;
+        float valor02;
+        boolean valor03;
+        char letra;
+        valor01 = 12;
+        valor02 = 12.5f;
+        valor03 = true;
+        letra = 'j';
+        System.out.println("Conteúdo de valor02 = " + valor02);
+    }
+}
