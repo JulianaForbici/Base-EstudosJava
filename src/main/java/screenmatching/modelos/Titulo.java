be2a36a16668feb8c9bbdcfde49e87d721ff866c
juliana.forbici <juliana.forbici@senior.com.br>
2026-02-12 09:09:36 -0300

    usado gson e utilizado api de filmes pra teste

--- src/main/java/screenmatching/modelos/Titulo.java
+++ src/main/java/screenmatching/modelos/Titulo.java
@@ -1,7 +1,11 @@
 package screenmatching.modelos;
 
+import com.google.gson.annotations.SerializedName;
+
 public class Titulo implements Comparable<Titulo>{
+    @SerializedName("Title")
     private String nome;
+    @SerializedName("Year")
     private int anoDeLancamento;
     private boolean incluindoNoPlano;
     private double somaAvaliacoes;
@@ -66,5 +70,10 @@ public class Titulo implements Comparable<Titulo>{
     @Override
     public int compareTo(Titulo o) {
         return this.getNome().compareTo(o.getNome());
+    }
+
+    @Override
+    public String toString() {
+        return "nome='" + nome + '\'' + ", anoDeLancamento=" + anoDeLancamento + '}';
     }
 }
